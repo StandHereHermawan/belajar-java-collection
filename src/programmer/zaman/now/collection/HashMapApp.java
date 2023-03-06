041d776caeb1559fa108d67fdfe4d15997531733
StandHereHermawan <nolifegansaya@gmail.com>
2023-03-06 08:33:45 +0700

    Map Interface, HashMap

---
+++ src/programmer/zaman/now/collection/HashMapApp.java
@@ -0,0 +1,18 @@
+package programmer.zaman.now.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapApp {
+    public static void main(String[] args) {
+
+        Map<String, String> map = new HashMap<>();
+        map.put("name.first", "Hilmi");
+        map.put("name.middle", "Akbar");
+        map.put("name.last", "Muharrom");
+
+        System.out.println(map.get("name.first"));
+        System.out.println(map.get("name.middle"));
+        System.out.println(map.get("name.last"));
+    }
+}
