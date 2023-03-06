1d739a07c297410230331fe36fc2b82c847d03c1
StandHereHermawan <nolifegansaya@gmail.com>
2023-03-06 19:56:48 +0700

    Some Part Java Collection

---
+++ src/programmer/zaman/now/collection/MapDefaultMethodApp.java
@@ -0,0 +1,22 @@
+package programmer.zaman.now.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+public class MapDefaultMethodApp {
+    public static void main(String[] args) {
+
+        Map<String, String> map = new HashMap<>();
+        map.put("Gorengan","2000");
+        map.put("Teh Gelas","2000");
+        map.put("Makaroni","2000");
+
+        map.forEach(new BiConsumer<String, String>() {
+            @Override
+            public void accept(String key, String value) {
+                System.out.println(key + " : " + value);
+            }
+        });
+    }
+}
